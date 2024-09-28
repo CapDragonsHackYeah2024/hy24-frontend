f2702b94bd027b067f6b82165bfcb228122f7174
Lavaret <czarnynozyk@gmail.com>
2024-09-28 17:25:20 +0200

    changed icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 // import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
+import MapIcon from '@mui/icons-material/Map';
 // import { useNavigate } from 'react-router-dom';
 import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 
@@ -29,7 +29,7 @@ export default function Navbar({tabs}) {
                     <ListItem key={index} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                {index % 2 === 0 ? <HomeIcon /> : <MapIcon />}
                             </ListItemIcon>
                             <Link href={tab.url} underline="none">
                                 {tab.text}
